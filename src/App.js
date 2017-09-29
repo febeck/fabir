2106364de972ae4e8ed186633e5df23f5863eacb
Fernando Beck <fe.beck25@gmail.com>
2017-09-29 19:11:47 -0300

    Add Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Highlighter from './Highlighter'
 
 class App extends Component {
@@ -25,22 +26,24 @@ class App extends Component {
 
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to FABIR</h1>
-        </header>
-        <p className='App-intro'>
-          Please insert a text and ask a question
-        </p>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <textarea rows={20} cols={80} placeholder={"Please enter a text"} value={this.state.text} onChange={this.handleChangeText} />
-          </div>
-        <input type="submit" value="Submit" />
-      </form>
-        <Highlighter text={this.state.text} highlightedText={this.state.answer} />
-      </div>
+      <MuiThemeProvider>
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h1 className='App-title'>Welcome to FABIR</h1>
+          </header>
+          <p className='App-intro'>
+            Please insert a text and ask a question
+          </p>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <textarea rows={20} cols={80} placeholder={"Please enter a text"} value={this.state.text} onChange={this.handleChangeText} />
+            </div>
+          <input type="submit" value="Submit" />
+        </form>
+          <Highlighter text={this.state.text} highlightedText={this.state.answer} />
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
